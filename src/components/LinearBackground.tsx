282700044793358cb9318da43160f6c76244fa4f
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-06-19 16:20:30 -0300

    chore: fix

--- src/components/LinearBackground.tsx
+++ src/components/LinearBackground.tsx
@@ -2,15 +2,17 @@ import React, { ReactNode } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import { theme } from '../global/styles/theme';
+import { StyleProp, ViewStyle } from 'react-native';
 
 type LinearBackgroundProps = {
   children: ReactNode;
+  style?: StyleProp<ViewStyle>;
 };
 
-export function LinearBackground({ children }: LinearBackgroundProps) {
+export function LinearBackground({ children, style }: LinearBackgroundProps) {
   return (
     <LinearGradient
-      style={{ flex: 1 }}
+      style={[{ flex: 1 }, style]}
       colors={[theme.backgroundGradient.from, theme.backgroundGradient.to]}
     >
       {children}
--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { AuthRoutes } from './auth.routes';
-import { LinearBackground } from '../components/LinearBackground';
 
 export function Routes() {
   return (
