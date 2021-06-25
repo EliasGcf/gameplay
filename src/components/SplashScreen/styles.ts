8d6076eed6de33b62bdb12f140599f903a8a64cf
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-06-24 23:37:06 -0300

    chore: add linear gradient to splash scree

--- src/components/SplashScreen/styles.ts
+++ src/components/SplashScreen/styles.ts
@@ -1,8 +1,10 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled(LinearGradient).attrs({
+  colors: ['#0E1647', '#0A1033'],
+})`
   flex: 1;
-  background: #0E1647;
   align-items: center;
   justify-content: center;
   position: relative;
