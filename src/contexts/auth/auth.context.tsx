9a957c2cbebed47c3c70ffec72baaf9800f782e5
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-06-26 20:46:32 -0300

    feat: save user data with async storage

--- src/contexts/auth/auth.context.tsx
+++ src/contexts/auth/auth.context.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, ReactNode, useState } from 'react';
+import React, { createContext, ReactNode, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as AuthSession from 'expo-auth-session';
 
 import { discordApi } from '../../services/discordApi';
@@ -13,9 +14,36 @@ import {
 
 export const AuthContext = createContext<AuthContextData | undefined>(undefined);
 
+const ASYNC_STORAGE_BASE_KEY = '@GamePlay';
+const ASYNC_STORAGE_TOKEN_KEY = `${ASYNC_STORAGE_BASE_KEY}:token` as const;
+const ASYNC_STORAGE_USER_KEY = `${ASYNC_STORAGE_BASE_KEY}:user` as const;
+
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [data, setData] = useState<AuthState>();
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function loadStorageData() {
+      const [tokenKeyAndValue, userKeyAndValue] = await AsyncStorage.multiGet([
+        ASYNC_STORAGE_TOKEN_KEY,
+        ASYNC_STORAGE_USER_KEY,
+      ]);
+
+      const [, token] = tokenKeyAndValue;
+      const [, user] = userKeyAndValue;
+
+      if (token && user) {
+        discordApi.defaults.headers.authorization = `Bearer ${token}`;
+
+        setData({
+          token,
+          user: JSON.parse(user),
+        });
+      }
+    }
+
+    loadStorageData();
+  }, []);
 
   async function signIn() {
     try {
@@ -24,10 +52,10 @@ export function AuthProvider({ children }: { children: ReactNode }) {
       const authUrl = discordApi.getUri({
         url: `${discordApi.defaults.baseURL}/oauth2/authorize`,
         params: {
+          scope: discordAuthConfig.scope,
           client_id: discordAuthConfig.clientId,
           redirect_uri: discordAuthConfig.redirectURL,
           response_type: discordAuthConfig.responseType,
-          scope: discordAuthConfig.scope,
         },
       });
 
@@ -51,6 +79,11 @@ export function AuthProvider({ children }: { children: ReactNode }) {
       };
 
       setData({ token, user });
+
+      await AsyncStorage.multiSet([
+        [ASYNC_STORAGE_TOKEN_KEY, token],
+        [ASYNC_STORAGE_USER_KEY, JSON.stringify(user)],
+      ]);
     } catch (error) {
       throw new Error(
         'Não foi possível realizar a autenticação, tente novamente mais tarde.',
