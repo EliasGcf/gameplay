67d40f05acac7190f7bb2d560fb3367b69eb2d21
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-06-26 23:57:48 -0300

    chore: add suport to absolute paths

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,19 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin', 'inline-dotenv'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      'inline-dotenv',
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@components': './src/components',
+            '@config': './src/config',
+          }
+        },
+      ],
+    ],
   };
 };
